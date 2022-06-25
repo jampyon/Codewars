475e6c9f356727273c18bd48b8cb67acaf0a796c
James Pyon <jamespyon1@gmail.com>
2022-06-25 17:28:38 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+//8 Kyu - Multiply
+// function multiply(a, b){
+//     const multiplication = a * b;
+//     return multiplication;
+//   }
+//   multiply(1, 1);
+////////////////////////////////////////////////////////////////////////////////////////////
+
+//8 Kyu - Are You Playing Banjo?
+// const areYouPlayingBanjo = function (name) {
+//     const banjoStatus = (name.startsWith("R") || name.startsWith("r")) ? `${name} plays banjo` : `${name} does not play banjo`;
+//     return banjoStatus;
+//   };
+////////////////////////////////////////////////////////////////////////////////////////////
+
+//8 Kyu - SumArrays
+// function sum (numbers) {
+//     "use strict";
+//     let sum = 0;
+//     for(let i = 0; i < numbers.length; i++) {
+//       sum += numbers[i];
+//     };
+//     return sum;
+// };
+////////////////////////////////////////////////////////////////////////////////////////////
