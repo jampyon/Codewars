11a6f0691ce2fb7e4c430d40720a4b5d85443e6e
James Pyon <jamespyon1@gmail.com>
2022-06-30 22:45:58 -0500

    Stuck on 7kyu High and Low

--- 7kyu.js
+++ 7kyu.js
@@ -14,3 +14,33 @@
 
 //7 Kyu - Highest and Lowest ()
 //Assignment: In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+// const str = "-19 2 3 44";
+// const returnArr = [];
+// function highAndLow(str) {
+//     for(let i = 0; i < str.length; i++) {
+//         if(str.charAt(i) === " ") {
+//             returnArr.push(parseInt(str));
+//         }
+//     }
+//     return returnArr;
+// }
+// console.log(highAndLow(str));
+//Note: STUUUUUUUUUUUUUUUUUUUUUUUUUCK
+////////////////////////////////////////////////////////////////////////////////////////////
+
+//7 Kyu - Exes and Ohs (Completed 6.29.22)
+//Assignment: Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+// function XO(str) {
+//     let xCounter = 0;
+//     let oCounter = 0;
+//     for(let i = 0; i < str.length; i++) {
+//       if(str.charAt(i) === "x" || str.charAt(i) === "X") {
+//         xCounter += 1;
+//       } else if(str.charAt(i) === "o" || str.charAt(i) === "O") {
+//         oCounter += 1;
+//       }
+//     }
+//     const boolChecker = (xCounter === oCounter) ? true : false;
+//     return boolChecker;
+// }
+//Notes: This took me too long to do. The issue was syntax with .charAt(). I kept running it while it said .charAt[] when the correct syntax is parentheses. Also, there's an easier method. Review the code next time.
