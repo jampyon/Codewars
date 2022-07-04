ce732b42e67c9ed8d656298e4481520da2aaae77
James <jamespyon1@gmail.com>
2022-07-03 23:57:53 -0500

    7kyu: completed backlogged challenges.

--- 7kyu.js
+++ 7kyu.js
@@ -12,20 +12,17 @@
 //Notes: This one was a bit rough, personally, because it took me a while to get the math down. I knew that I needed to calculate with the length of the param/arg, but it took me a while. Also, my solution isn't DRY. For the if/else if statement, I should've used something else.
 ////////////////////////////////////////////////////////////////////////////////////////////
 
-//7 Kyu - Highest and Lowest ()
+//7 Kyu - Highest and Lowest (Completed 7.3.2022)
 //Assignment: In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
-// const str = "-19 2 3 44";
-// const returnArr = [];
-// function highAndLow(str) {
-//     for(let i = 0; i < str.length; i++) {
-//         if(str.charAt(i) === " ") {
-//             returnArr.push(parseInt(str));
-//         }
+// function highAndLow(numbers){
+//     const argConvert = numbers.match(/(-\d+|\d+)(,\d+)*(\.\d+)*/g);
+//     const numArray = [];
+//     for(let i = 0; i < argConvert.length; i++) {
+//         numArray.push(Number(argConvert[i]));
 //     }
-//     return returnArr;
+//     return `${Math.max(...numArray)} ${Math.min(...numArray)}`;
 // }
-// console.log(highAndLow(str));
-//Note: STUUUUUUUUUUUUUUUUUUUUUUUUUCK
+//Note: Used regex (numbers.match(/(-\d+|\d+)(,\d+)*(\.\d+)*/g)) to extract all the numbers into the argConvert variable. Math.max/min syntax is different for arrays. It needs the ellipses (...arrName) to work. What the best solutions used is var.split(" "); and then used Math.max/min to get the answers.
 ////////////////////////////////////////////////////////////////////////////////////////////
 
 //7 Kyu - Exes and Ohs (Completed 6.29.22)
@@ -44,3 +41,23 @@
 //     return boolChecker;
 // }
 //Notes: This took me too long to do. The issue was syntax with .charAt(). I kept running it while it said .charAt[] when the correct syntax is parentheses. Also, there's an easier method. Review the code next time.
+////////////////////////////////////////////////////////////////////////////////////////////
+
+//7 Kyu - Odds or Evens (Completed 7.3.22)
+// function oddOrEven(array) {
+//     let sum = 0;
+//     for(let i = 0; i < array.length; i++) {
+//       sum += array[i];
+//     }
+//     return (sum % 2 === 0 || sum % 2 === -0) ? "even" : "odd";
+//  }
+//Notes: The issue I was having was that adding and using the remainder operator (%) with negative numbers was returning -0, which wasn't accounted in the conditional/ternary operator.
+////////////////////////////////////////////////////////////////////////////////////////////
+
+//7 Kyu - Sum of odd numbers (Completed 7.3.22)
+// function rowSumOddNumbers(n) {
+//     return n * n * n;
+//   }
+//Notes: Pyramid of consecutive odd integers. Row 1 was 1, Row 2 was 3 and 5...The problem was to find the sum of nth row and return the sum. I don't know why this worked, but it worked.
+////////////////////////////////////////////////////////////////////////////////////////////
+
